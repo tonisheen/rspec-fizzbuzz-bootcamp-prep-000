5a42c2db85672749932ac6b488aed93184ef1610
Newton Migosi <newtonmigosi@gmail.com>
2018-05-01 08:53:29 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,9 @@
 def fizzbuzz(num)
   if num % 3 == 0
     "Fizz"
-  end
-  if num % 5 == 0
+  elsif num % 5 == 0
     "Buzz"
-  end
-  if num % 15 == 0
+  elsif num % 15 == 0
     "fizzBuzz"
   end
 end
